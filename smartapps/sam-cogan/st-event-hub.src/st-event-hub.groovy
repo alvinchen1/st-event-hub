d94b5d9550c7cbc0fd2ca2e761d1043fc108487a
alvinchen1 <github.interest@alvinchen.net>
2019-02-26 07:41:55 -0500

    Version 1.1    Currently testing temperature, humidity, accleration, and presence sensors, by displaying debug

--- smartapps/sam-cogan/st-event-hub.src/st-event-hub.groovy
+++ smartapps/sam-cogan/st-event-hub.src/st-event-hub.groovy
@@ -12,6 +12,10 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *  Version
+ *  1.1    Currently testing temperature, humidity, accleration, and presence sensors, by displaying debug
+ *
+ *
  */
 definition(
     name: "ST-Event-Hub",
